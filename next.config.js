ca67f427ff9ab2e4f5f90fe8d1e2ab5e2bea60f2
Phelipe de Brito Silva <69465563+PhelipeBrito@users.noreply.github.com>
2021-02-18 13:39:02 -0300

    configurando para aceitar svg

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-    trailingSlash: true
+    trailingSlash: true,
+    webpack(config) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          issuer: {
+            test: /\.(js|ts)x?$/,
+          },
+          use: ['@svgr/webpack'],
+        });
+    
+        return config;
+      },
 }
--- src/components/PokedexHeader/index.js
+++ src/components/PokedexHeader/index.js
@@ -3,18 +3,31 @@ import styled from 'styled-components';
 const Header = styled.header`
     width: 100%;
     height: 5vh;
+    padding: 4px;
+
+    display: flex;
+    align-items: center;
 
     background-color: #555555;
+`
+
+Header.ButtonListToggle = styled.button`
+    background: none;
+    border: none;
+
+    font-size: 20px;
+    font-weight: bold;
+    color: #ffffff;
 `
 
 function PokedexHeader({listToggle}) {
     return (
         <Header>
-            <button
+            <Header.ButtonListToggle
                 onClick={(() => listToggle())}
             >
-                Toggle
-            </button>
+                List
+            </Header.ButtonListToggle>
         </Header>
     )
 }
